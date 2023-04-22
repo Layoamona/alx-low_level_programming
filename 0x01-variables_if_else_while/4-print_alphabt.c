1fb1ab44ee58fcddb9e175b856ff26e852b220a9
Layoamona <amonamarysmith@gmail.com>
2023-04-22 13:17:45 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that print alphabeth inlowercase,
+ * followed by a new line, except q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a'
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
