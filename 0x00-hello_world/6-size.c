22170b2830875801fba02456a24009a77f246b0c
Layoamona <amonamarysmith@gmail.com>
2023-04-22 03:05:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-
-printf("Size of a char: %zu byte($)\n", sizeof(a));
-printf("Size of an int: %zu byte($)\n", sizeof(b));
-printf("Size of a long int: %zu byte($)\n", sizeof(c));
-printf("Size of a long long int: %zu byte($)\n", sizeof(d));
-printf("Size of a float: %zu byte($)\n", sizeof(f));
+printf("Size of a char: %zu byte($)\n", sizeof(char));
+printf("Size of an int: %zu byte($)\n", sizeof(int));
+printf("Size of a long int: %zu byte($)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte($)\n", sizeof(long long int));
+printf("Size of a float: %zu byte($)\n", sizeof(float));
 return (0);
 }
