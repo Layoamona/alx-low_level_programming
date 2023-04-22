81abce29cb28f305b4a801a2d8ad3004455cd7f5
Layoamona <amonamarysmith@gmail.com>
2023-04-22 02:36:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Return: 0 (success)
+ *main - A program that prints the size of various computer types
+ *Return: 0 (success)
  */
- int main(void)
+int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
-	printf("Size of a char: %zu byte($)\n", sizeof(a));
-	printf("Size of an int: %zu byte($)\n", sizeof(b));
-	printf("Size of a long int: %zu byte($)\n", sizeof(c));
-	printf("Size of a long long int: %zu byte($)\n", sizeof(d));
-	printf("Size of a float: %zu byte($)\n", sizeof(f));
-	return (0);
+printf("Size of a char: %d byte($)\n", sizeof(a));
+printf("Size of an int: %d byte($)\n", sizeof(b));
+printf("Size of a long int: %d byte($)\n", sizeof(c));
+printf("Size of a long long int: %d byte($)\n", sizeof(d));
+printf("Size of a float: %d byte($)\n", sizeof(f));
+return (0);
 }
