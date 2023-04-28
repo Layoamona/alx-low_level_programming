cf5d456e941e775fa9fce5759738e470295f385b
Layoamona <amonamarysmith@gmail.com>
2023-04-27 17:41:01 -0700

    Debugging files

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Causes an infinte loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/*while (i < 10)*/
+	/*}*/
+	
+	printf("infinte loop avoided! \\o/\n");
+	return (0);
+}
