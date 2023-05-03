e20808f6ce89e29b47a9f183dbf0317c23b9071e
Layoamona <amonamarysmith@gmail.com>
2023-05-02 17:12:36 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the variable
+ * the pointer points to 98
+ * @n: pointer to the variable to udate
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
