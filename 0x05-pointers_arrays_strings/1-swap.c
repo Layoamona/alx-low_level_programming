10c05362711f426f5168f6f9eda0ab3be5573511
Layoamona <amonamarysmith@gmail.com>
2023-05-02 19:51:51 -0700

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int Function that swaps the value of two integers
+ * swap_int - Function that swaps the value of two integers
  * @a: pointer to the first value
  * @b: pointer to the second value
  */
