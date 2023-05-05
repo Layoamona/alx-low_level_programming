cb7820eed6ab9885c3206cf71dc63b2e790cf020
Layoamona <amonamarysmith@gmail.com>
2023-05-04 21:53:03 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rot13 - function that encodes a string using rot13
+ * rot13 - function that encodes a string in rot13
  * @s: string to be encoded
  * Return: resulting string
  */
